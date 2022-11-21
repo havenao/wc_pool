f24bc36363296f226b697d213e7f45a8943ff3f1
Haven Schrock <havenao@gmail.com>
2022-11-21 17:34:14 -0500

    qol updates to UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def self.strip_trailing_zero(n)
+    n.to_s.sub(/\.?0+$/, '')
+  end
 end
--- app/models/share.rb
+++ app/models/share.rb
@@ -19,6 +19,11 @@ class Share < ApplicationRecord
     player_share.to_f / total_shares.to_f
   end
 
+  def self.get_points(team_id, player_id)
+    team = Team.find(team_id)
+    team.points * calculate_value(team_id, player_id)
+  end
+
   def get_team_name(id)
     Team.find(id).name
   end
