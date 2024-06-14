a12b39c6f4c98b070d2ff25abb163217643bcf7a
havenurelm <haven@nurelm.com>
2024-06-14 16:23:26 -0400

    add total team shares

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,4 +7,8 @@ class Team < ApplicationRecord
   def points
     @points ||= Result.where(team_id: self.id).inject(0) { |total, result| total + result.points } || 0
   end
+
+  def total_shares
+    @total_shares ||= shares.inject(0) {| i,s| i = i + s.amount}
+  end
 end
